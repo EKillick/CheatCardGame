2d415aa078e5b504a1fe3113b70bc79521365c1a
EKillick <edwinkillick@gmail.com>
2017-01-20 23:31:01 +0000

    Card class updates

--- src/cheatcardgame/Card.java
+++ src/cheatcardgame/Card.java
@@ -6,25 +6,77 @@ import java.io.Serializable;
  *
  * @author 6277497
  */
-public class Card implements Serializable {
+public class Card implements Serializable, Comparable {
     static final long serialVersionUID = 100;
     
-    private enum Rank{
-        TWO (2),THREE (3), FOUR (4), FIVE (5), SIX (6), SEVEN (7), EIGHT (8),
-        NINE (9), TEN (10), JACK (10), QUEEN (10), KING (10), ACE (11)
-//        private static Rank[] valArray = values();
-//        public Rank getNext()
-//        {
-//            return valArray[(this.ordinal()+1) % valArray.length];
-//        }
+    /**
+     * An enum representing all the values a card can have
+     */
+    protected enum Rank{
+        TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8),
+        NINE(9), TEN(10), JACK(10), QUEEN(10), KING(10), ACE(11);
+        final int value;
+        Rank(int x){
+            value = x;
+        }
+        
+        /*
+        An array of all ranks to be used in the getNext method
+        */
+        private static final Rank[] rankValues = values(); 
+        
+        /*
+        A function that returns the next enum value to the one given
+        */
+        public Rank getNext(){
+            return rankValues[(this.ordinal()+1) % rankValues.length];
+        }
     }
-    private enum Suit{
+
+    /**
+     * An enum representing all the suits a card can have
+     */
+    protected enum Suit{
         CLUB, DIAMONDS, HEARTS, SPADES
     }
+    
+    private Rank rank;
+    private Suit suit;
     
     public Card(Rank rank, Suit suit){
         this.rank = rank;
         this.suit = suit;
     }
     
+    /*
+    An accessor method to return the rank of a given card
+    */
+    public Rank getRank(){
+        return this.rank;
+    }
+    
+    /*
+    An accessor method to return the suit of a given card
+    */
+    public Suit getSuit(){
+        return this.suit;
+    }
+    
+    //Prints out the value and suit of a given card
+    @Override
+    public String toString(){
+        StringBuilder returnString = new StringBuilder();
+        returnString.append("Card Value: ");
+        returnString.append(rank);
+        returnString.append("\nCard Suit: ");
+        returnString.append(suit);
+        
+        return returnString.toString();
+    }
+    
+    //To be implemented
+    @Override
+    public int compareTo(Object t) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
--- src/cheatcardgame/CheatCardGame.java
+++ src/cheatcardgame/CheatCardGame.java
@@ -1,5 +1,4 @@
 package cheatcardgame;
-
 /**
  *
  * @author 6277497
@@ -7,7 +6,9 @@ package cheatcardgame;
 public class CheatCardGame {
 
     public static void main(String[] args) {
-//        Card twoOfHearts = new Card(Rank.TWO, Suit.CLUBS);
+    Card twoOfHearts = new Card(Card.Rank.TWO, Card.Suit.CLUB);
+    System.out.println(twoOfHearts.toString());
+    System.out.println(Card.Rank.ACE.getNext());
     }
     
 }
