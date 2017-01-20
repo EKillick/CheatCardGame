9a4c765c5ecc558aabbe739506802dfa6b8abd86
EKillick <edwinkillick@gmail.com>
2017-01-20 18:00:43 +0000

    Added suit and rank enums

--- src/cheatcardgame/Card.java
+++ src/cheatcardgame/Card.java
@@ -1,9 +1,20 @@
 package cheatcardgame;
 
+import java.io.Serializable;
+
 /**
  *
  * @author 6277497
  */
-public class Card {
+public class Card implements Serializable {
+    static final long serialVersionUID = 100;
+    
+    private enum Rank{
+        TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, 
+        QUEEN, KING, ACE
+    }
+    private enum Suit{
+        CLUB, DIAMONDS, HEARTS, SPADES
+    }
     
 }
